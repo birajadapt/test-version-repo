c14e3c79ab55aea6054e556c461f4e4f6d90bc1c
Biraj <biraj.patel@getadapt.ai>
2024-09-05 12:42:47 +0530

    fix: only get first tag

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import subprocess
 
-version = subprocess.run(["git", "tag"], text=True, capture_output=True).stdout.strip()
+version = (
+    subprocess.run(["git", "tag"], text=True, capture_output=True)
+    .stdout.strip()
+    .splitlines()[0]
+)
 
 print("version is", version)
 
