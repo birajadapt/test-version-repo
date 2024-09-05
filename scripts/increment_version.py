8ac32264c1a78dc2535604d664c20cfc1496a47e
Biraj <biraj.patel@getadapt.ai>
2024-09-05 22:58:26 +0530

    feat: update again

--- scripts/increment_version.py
+++ scripts/increment_version.py
@@ -16,10 +16,10 @@ def determine_increment_type(commit_message: str) -> str:
 
     if re.match(r"^(?:chore|docs|style|refactor|build|ci|test):", commit_message):
         return None
-    if re.search(r"\bBREAKING CHANGE\b", commit_message):
-        return "major"
     elif re.match(r"^feat:", commit_message):
         return "minor"
+    if re.search(r"\bBREAKING CHANGE\b", commit_message):
+        return "major"
     # elif re.match(r"^fix:", commit_message):
     #     return "patch"
     else:
@@ -33,22 +33,26 @@ def main():
 
     commit_message = sys.argv[1]
 
-    with open("version.txt", "r") as f:
-        current_version = f.read().strip()
-
-    print(f"Current version: {current_version}")
     print(f"Commit message: {commit_message}")
 
-    increment_type = determine_increment_type(commit_message)
-    print(f"Increment type: {increment_type}")
-    if increment_type is None:
-        return current_version
+    with open("version.txt", "r+") as f:
+        current_version = f.read().strip()
 
-    new_version = semver.VersionInfo.parse(current_version).next_version(
-        part=increment_type
-    )
+        print(f"Current version: {current_version}")
 
-    print(new_version)
+        increment_type = determine_increment_type(commit_message)
+        print(f"Increment type: {increment_type}")
+        if increment_type is None:
+            return current_version
+
+        new_version = semver.VersionInfo.parse(current_version).next_version(
+            part=increment_type
+        )
+
+        f.seek(0)
+        f.write(f"{str(new_version)}\n")
+
+        print(new_version)
 
 
 if __name__ == "__main__":
